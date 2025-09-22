f7a3db6ea2494be4b55eca5fd4e6b08208515174
Saif-hadd <haddedseif746@gmail.com>
2025-09-22 22:49:34 +0200

    last commit

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Point d'entrée principal de l'application React
+// Initialise React et monte l'application sur l'élément #root
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
